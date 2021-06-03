26680910bc6123ffa0e0c51778d0e614994a2d7a
窮奇野闇冥 <79628447+kyuukino-anmei@users.noreply.github.com>
2021-06-03 17:35:18 +0800

    Add files via upload

---
+++ test/p5js-temp-sketch_210603g9205300619202362049.js
@@ -0,0 +1,20 @@
+function setup()
+{
+  createCanvas(400, 200); 
+}
+function setup()
+{
+  createCanvas(400, 200); 
+}
+function draw()
+{
+  let s=second();
+  if(s%2==0) 
+  {
+    background(255, 13, 74);
+  }
+  else 
+  {
+     background(13, 249, 255);
+  }  
+}
